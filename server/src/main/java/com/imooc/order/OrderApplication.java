cd72c96e1001d1645b34341e2c44d2da9068f528
zhangzhengguo <zzg@hugehuge.cn>
2019-09-25 10:01:10 +0800

    项目多模块化

--- server/src/main/java/com/imooc/order/OrderApplication.java
+++ server/src/main/java/com/imooc/order/OrderApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableFeignClients
+@EnableFeignClients(basePackages = "com.imooc.product.client")
 public class OrderApplication {
 
     public static void main(String[] args) {
